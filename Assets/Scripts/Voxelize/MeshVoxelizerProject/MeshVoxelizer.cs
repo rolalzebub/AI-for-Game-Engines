f4f02f47629e693620a2425522e991a26839c015
rolalzebub <rohit.lalwani1996@hotmail.com>
2022-03-08 15:21:28 +0000

    mesh voxelizer doneee

---
+++ Assets/Scripts/Voxelize/MeshVoxelizerProject/MeshVoxelizer.cs
@@ -0,0 +1,249 @@
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+//credit to:
+//https://github.com/Scrawk/Mesh-Voxelization
+namespace MeshVoxelizerProject
+{
+    public class MeshVoxelizer
+    {
+
+        public int Count { get; private set; }
+
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public int Depth { get; private set; }
+
+        public int[,,] Voxels { get; private set; }
+
+        public List<Box3> Bounds { get; private set; }
+
+        public MeshVoxelizer(int width, int height, int depth)
+        {
+
+            Width = width;
+            Height = height;
+            Depth = depth;
+            Bounds = new List<Box3>();
+            Voxels = new int[width, height, depth];
+        }
+
+        public Mesh Voxelize(IList<Vector3> vertices, IList<int> indices, Box3 bounds)
+        {
+            Array.Clear(Voxels, 0, Voxels.Length);
+
+            // build an aabb tree of the mesh
+            MeshRayTracer tree = new MeshRayTracer(vertices, indices);
+            Bounds = tree.GetBounds();
+
+            // parity count method, single pass
+            Vector3 extents = bounds.Size;
+            Vector3 delta = new Vector3(extents.x / Width, extents.y / Height, extents.z / Depth);
+            Vector3 offset = new Vector3(0.5f / Width, 0.5f / Height, 0.5f / Depth);
+
+            //1e-7f is magic number for float-to-zero comparison
+            float eps = 1e-7f * extents.z;
+
+            for (int x = 0; x < Width; ++x)
+            {
+                for (int y = 0; y < Height; ++y)
+                {
+                    bool inside = false;
+                    Vector3 rayDir = new Vector3(0.0f, 0.0f, 1.0f);
+
+                    // z-coord starts somewhat outside bounds 
+                    Vector3 rayStart = bounds.Min + new Vector3(x * delta.x + offset.x, y * delta.y + offset.y, -0.0f * extents.z);
+
+                    while (true)
+                    {
+                        MeshRay ray = tree.TraceRay(rayStart, rayDir);
+
+                        if (ray.hit)
+                        {
+                            // calculate cell in which intersection occurred
+                            float zpos = rayStart.z + ray.distance * rayDir.z;
+                            float zhit = (zpos - bounds.Min.z) / delta.z;
+
+                            int z = (int)((rayStart.z - bounds.Min.z) / delta.z);
+                            int zend = (int)Math.Min(zhit, Depth - 1);
+
+                            if (inside)
+                            {
+                                for (int k = z; k <= zend; ++k)
+                                {
+                                    Voxels[x, y, k] = 1;
+                                    Count++;
+                                }
+                            }
+
+                            inside = !inside;
+                            rayStart += rayDir * (ray.distance + eps);
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+
+            Vector3 scale = new Vector3(bounds.Size.x / Width, bounds.Size.y / Height, bounds.Size.z / Depth);
+            Vector3 m = new Vector3(bounds.Min.x, bounds.Min.y, bounds.Min.z);
+
+            return CreateMesh(Voxels, scale, m);
+        }
+
+        #region Utilities
+
+        private void AddRightQuad(List<Vector3> verts, List<int> indices, Vector3 scale, Vector3 pos)
+        {
+            int count = verts.Count;
+
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 0 * scale.z));
+
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 0 * scale.z));
+
+            indices.Add(count + 2); indices.Add(count + 1); indices.Add(count + 0);
+            indices.Add(count + 5); indices.Add(count + 4); indices.Add(count + 3);
+        }
+
+        private void AddLeftQuad(List<Vector3> verts, List<int> indices, Vector3 scale, Vector3 pos)
+        {
+            int count = verts.Count;
+
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 0 * scale.z));
+
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 0 * scale.z));
+
+            indices.Add(count + 0); indices.Add(count + 1); indices.Add(count + 2);
+            indices.Add(count + 3); indices.Add(count + 4); indices.Add(count + 5);
+        }
+
+        private void AddTopQuad(List<Vector3> verts, List<int> indices, Vector3 scale, Vector3 pos)
+        {
+            int count = verts.Count;
+
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 0 * scale.z));
+
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 0 * scale.z));
+
+            indices.Add(count + 0); indices.Add(count + 1); indices.Add(count + 2);
+            indices.Add(count + 3); indices.Add(count + 4); indices.Add(count + 5);
+        }
+
+        private void AddBottomQuad(List<Vector3> verts, List<int> indices, Vector3 scale, Vector3 pos)
+        {
+            int count = verts.Count;
+
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 0 * scale.z));
+
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 0 * scale.z));
+
+            indices.Add(count + 2); indices.Add(count + 1); indices.Add(count + 0);
+            indices.Add(count + 5); indices.Add(count + 4); indices.Add(count + 3);
+        }
+
+        private void AddFrontQuad(List<Vector3> verts, List<int> indices, Vector3 scale, Vector3 pos)
+        {
+            int count = verts.Count;
+
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 1 * scale.z));
+
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 1 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 1 * scale.z));
+
+            indices.Add(count + 2); indices.Add(count + 1); indices.Add(count + 0);
+            indices.Add(count + 5); indices.Add(count + 4); indices.Add(count + 3);
+        }
+
+        private void AddBackQuad(List<Vector3> verts, List<int> indices, Vector3 scale, Vector3 pos)
+        {
+            int count = verts.Count;
+
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(0 * scale.x, 0 * scale.y, 0 * scale.z));
+
+            verts.Add(pos + new Vector3(0 * scale.x, 1 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 1 * scale.y, 0 * scale.z));
+            verts.Add(pos + new Vector3(1 * scale.x, 0 * scale.y, 0 * scale.z));
+
+            indices.Add(count + 0); indices.Add(count + 1); indices.Add(count + 2);
+            indices.Add(count + 3); indices.Add(count + 4); indices.Add(count + 5);
+        }
+
+        private Mesh CreateMesh(int[,,] voxels, Vector3 scale, Vector3 min)
+        {
+            List<Vector3> verts = new List<Vector3>();
+            List<int> indices = new List<int>();
+
+            for (int z = 0; z < Depth; z++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < Width; x++)
+                    {
+                        if (voxels[x, y, z] != 1) continue;
+
+                        Vector3 pos = min + new Vector3(x * scale.x, y * scale.y, z * scale.z);
+
+                        if (x == Width - 1 || voxels[x + 1, y, z] == 0)
+                            AddRightQuad(verts, indices, scale, pos);
+
+                        if (x == 0 || voxels[x - 1, y, z] == 0)
+                            AddLeftQuad(verts, indices, scale, pos);
+
+                        if (y == Height - 1 || voxels[x, y + 1, z] == 0)
+                            AddTopQuad(verts, indices, scale, pos);
+
+                        if (y == 0 || voxels[x, y - 1, z] == 0)
+                            AddBottomQuad(verts, indices, scale, pos);
+
+                        if (z == Depth - 1 || voxels[x, y, z + 1] == 0)
+                            AddFrontQuad(verts, indices, scale, pos);
+
+                        if (z == 0 || voxels[x, y, z - 1] == 0)
+                            AddBackQuad(verts, indices, scale, pos);
+                    }
+                }
+            }
+
+            if (verts.Count > 65000)
+            {
+                Debug.Log("Mesh has too many verts. You will have to add code to split it up.");
+                return new Mesh();
+            }
+
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(verts);
+            mesh.SetTriangles(indices, 0);
+
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+
+            return mesh;
+        }
+    }
+    #endregion
+}
