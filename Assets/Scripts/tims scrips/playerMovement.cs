1300a8e5ce1d007821282aa33ce7b6192990eb25
tim7882 <32070347+tim7882@users.noreply.github.com>
2022-03-14 14:50:33 +0000

    player

---
+++ Assets/Scripts/tims scrips/playerMovement.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    //Variables 
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+
+    //Referances
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();  
+
+    }
+
+    private void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+
+        }
+
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3( 0, 0, moveZ);
+        moveDirection *= walkSpeed;
+        if (isGrounded)
+        {
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                //walk
+                Walk();
+
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                //run
+                Run();
+
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                //idle
+                Idle();
+
+            }
+            moveDirection *= moveSpeed;
+        }
+        
+
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+
+    private void Idle()
+    {
+
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+
+    }
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+
+    }
+}
